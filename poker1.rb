ade11477dc72276ae36ef85f7a4b4bd86d559b3e
Josh Gwinn <jdgwinn911@gmail.com>
2018-09-17 10:56:17 -0400

    first error/fail

--- poker1.rb
+++ poker1.rb
@@ -2,6 +2,8 @@
 class Cards
     def initialize(number,shapes)
         @card_name = "#{number}#{shapes}"
+        @card_value = "#{number}"
     end
     attr_reader :card_name
+    
 end
--- poker_test.rb
+++ poker_test.rb
@@ -6,4 +6,13 @@ class Poker1 < Minitest::Test
         x = Cards.new("3", "D")
         assert_equal("3D", x.card_name)
     end
+    
+    def test_card_value
+        x = Cards.new("3", "H")
+        assert_equal("3", x.card_value)
+    end
+
+
+
+
 end
