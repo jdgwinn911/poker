b56ab745abd6d6f34c85b3983ae565e45b3da9d5
Josh Gwinn <jdgwinn911@gmail.com>
2018-11-01 11:50:25 -0400

    passed the 2nd test for gameplay

--- poker_test.rb
+++ poker_test.rb
@@ -360,7 +360,7 @@ class Poker1 < Minitest::Test
     temp.add_dem_cards_to_da_hand(Cards.new("8", "H"))
     temp.add_dem_cards_to_da_hand(Cards.new("9", "S"))
     game = Rules.new
-    assert_equal("Player1 Won!", game.play_game(temp, temp2))
+    assert_equal("Player2 Won!", game.play_game(temp, temp2))
     end
 
 end
