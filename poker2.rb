e6f0a72b31262ba981260b7e86ea0fda52d83baa
Josh Gwinn <jdgwinn911@gmail.com>
2018-11-15 11:51:55 -0500

    now is returning a random hand from the deck which was shuffled

--- poker2.rb
+++ poker2.rb
@@ -1,6 +1,27 @@
-class Deck #efficient way of technically doing a double loop and populating cards with values and suits into a deck without having class for Cards
+class Game # a class that does what 2 classes used to do =)
+    def initialize()
+    end
     CARDVAL = %i[two three four five six seven eight nine ten jack queen king ace]
     CARDSUIT = %i[spades clubs diamonds hearts]
-    Card = Struct.new(:number, :shape)
-    CARDVAL.flat_map { |number| CARDSUIT.map{|shape| Card.new(number, shape)}}
+    Card = Struct.new(:number, :shape) do
+        def to_s() # redefine to_s  to bring back cards as strings
+            "#{number.capitalize} of #{shape.capitalize}"
+        end
+        def ==(other) # avoiding dynamic constant error by looking value instead of object id
+            self.number == other.number &&
+            self.shape == other.shape
+        end
+    end
+
+
+     deck = CARDVAL.flat_map { |number| CARDSUIT.map{|shape| Card.new(number, shape)}} # basically a double loop to flatten the mapped arrays to give 52 cards back
+     shuffled = deck.shuffle
+
+     hand = []
+     5.times do
+        hand << shuffled.pop().to_s
+     end
+     puts hand
 end
+
+
