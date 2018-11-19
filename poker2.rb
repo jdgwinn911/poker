efd66339d30554435b4dda2cc4e0e33387355af0
Josh Gwinn <jdgwinn911@gmail.com>
2018-11-19 13:37:42 -0500

    refactoring poker in an OO way

--- poker2.rb
+++ poker2.rb
@@ -1,7 +1,7 @@
-class Game # a class that does what 2 classes used to do =)
+class Card
     def initialize()
     end
-    CARDVAL = %i[two three four five six seven eight nine ten jack queen king ace]
+    CARDVAL = %i[2 3 4 5 6 7 8 9 10 11 12 13 14]
     CARDSUIT = %i[spades clubs diamonds hearts]
     Card = Struct.new(:number, :shape) do
         def to_s() # redefine to_s  to bring back cards as strings
@@ -12,16 +12,40 @@ class Game # a class that does what 2 classes used to do =)
             self.shape == other.shape
         end
     end
+end
 
 
-     deck = CARDVAL.flat_map { |number| CARDSUIT.map{|shape| Card.new(number, shape)}} # basically a double loop to flatten the mapped arrays to give 52 cards back
-     shuffled = deck.shuffle
 
+class Deck < Card
+    def initialize()
+    @deck = CARDVAL.flat_map { |number| CARDSUIT.map{|shape| Card.new(number, shape)}} 
+     @shuffled = @deck.shuffle
+    end
+    
+    
+
+    def deal_hand()
      hand = []
      5.times do
-        hand << shuffled.pop().to_s
+        hand << @shuffled.pop().to_s
      end
      puts hand
+    end
+    attr_reader :deck
+    attr_reader :shuffled
 end
+
+class Hand < Deck
+    def initialize()
+    end
+    d = Deck.new
+    white = d.deal_hand()
+    puts "------"
+    black = d.deal_hand()
+
+    # attr_reader :deck
+    # attr_reader :shuffled
+end
+
 
 
