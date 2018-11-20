6a6b55b8e052305d125027fcb445e7c1e460719c
Josh Gwinn <jdgwinn911@gmail.com>
2018-11-19 23:49:42 -0500

    sorry for inconsistent pushes, added some functions and slightly restructured things

--- poker2.rb
+++ poker2.rb
@@ -2,36 +2,30 @@ class Cards
     def initialize(value, suit)
     end
     CARDVAL = %i[2 3 4 5 6 7 8 9 10 11 12 13 14]
-    CARDSUIT = %i[spades clubs diamonds hearts]
+    CARDSUIT = %i[Spades Clubs Diamonds Hearts]
     Card = Struct.new(:value, :suit) do
         def to_s() # redefine to_s  to bring back cards as strings
             "#{value.capitalize} of #{suit.capitalize}"
         end
         def ==(other) # avoiding dynamic constant error by looking value instead of object id
-            self.value == other.value &&
-            self.suit == other.suit
+            self.value == other.value
         end
     end
 end
 
-
 class Deck < Cards
     def initialize()
+    @hand = []
     @deck = CARDVAL.flat_map { |value| CARDSUIT.map{|suit| Card.new(value, suit)}} 
      @shuffled = @deck.shuffle
     end
-
-    def deal_card(card)
-        @hand = []
-        @hand << card
-    end
-
     
     def deal_hand()
+        hand = Hand.new
      5.times do
-        deal_card(@shuffled.pop().to_s)
+        hand << @shuffled.pop()
      end
-     puts @hand
+     return hand
     end
     attr_reader :deck
     attr_reader :shuffled
@@ -40,35 +34,33 @@ end
 
 class Hand < Deck
     def initialize()
+        @cards = []
     end
 
-    def phand(hand)
-        puts "#{hand}"
+    def to_s()
+        stwing = ""
+        @cards.each do |v|
+            stwing += "#{v}\n"
+        end
+        return stwing
+    end
+    def <<(other_card)
+        @cards << other_card
     end
 
-    ranks = {
-        straight_flush:  8,
-        four_of_a_kind:  7,
-        full_house:      6,
-        flush:           5,
-        straight:        4,
-        three_of_a_kind: 3,
-        two_pair:        2,
-        pair:            1
+    def hand_pair() # must return true 
+
+    end
+
+    ranks = {straight_flush:  8, four_of_a_kind: 7, full_house: 6,
+        flush:5, straight: 4, three_of_a_kind: 3,
+        two_pair: 2, pair: 1
     }.freeze
-
-
-    p 
-
-
-    
-
-    d = Deck.new
-    white = d.deal_hand()
-    puts "------"
-    black = d.deal_hand()
-
 end
+d = Deck.new
+white = d.deal_hand(); puts white
+puts "------"
+black = d.deal_hand(); puts black
 
 
 
--- poker_test2.rb
+++ poker_test2.rb
@@ -5,12 +5,12 @@ class Poker2 < Minitest::Test
     def test_that_hand_is_getting_cards
     temp = Hand.new
     temp.deal_card(Cards.new("5", "S"))
-    temp.deal_card(Cards.new("4", "S"))
-    temp.deal_card(Cards.new("6", "S"))
+    temp.deal_card(Cards.new("4", "H"))
+    temp.deal_card(Cards.new("6", "C"))
+    temp.deal_card(Cards.new("8", "D"))
     temp.deal_card(Cards.new("8", "S"))
-    temp.deal_card(Cards.new("7", "S"))
     game = Hand.new
-    assert_equal("8 of Spades", game.phand(temp))
+    assert_equal("8 of Spades", game.pair?(temp))
     end
 end
 
